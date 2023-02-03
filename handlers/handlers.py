d17d1f5046703f7604d1244f06cbd96c94574339
Rembish Andrii <rembish10@gmail.com>
2023-02-03 22:39:06 +0200

    finish commit, change handlers.py

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,13 +1,10 @@
-import time
 from aiogram import types
-from aiogram.types import CallbackQuery
-from main import dp
-
-
-
 from main import bot, dp
-from settings import admin_id, MSG
-# from autokrislo_bot.data.db_map import create_profile
+from settings import admin_id
+from src.database.create_table import execute_query
+from zoneinfo import ZoneInfo
+from datetime import datetime
+
 
 
 async def send_to_admin(dp):
@@ -17,24 +14,45 @@ async def send_to_admin(dp):
 
 
 
-@dp.message_handler(commands=["start"])
+@dp.message_handler(commands=["/start", "/help"])
 async def start_handler(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
     user_id = message.from_user.id
     first_name = message.from_user.first_name
     last_name = message.from_user.last_name
     user_full_name = message.from_user.full_name
     # create_profile(id=user_id, f_name=first_name, l_name=last_name, info_i=user_full_name)
-    await bot.send_message(user_id, text="dddddddddddd")
+    timestamp_now = datetime.now(tz=ZoneInfo("UTC")).isoformat(" ")
+    insert_query = (f"INSERT INTO message (message, userid, message_time) "
+                    f"VALUES ({message.text}, {message.from_user['id']}, {timestamp_now})")
+    execute_query(insert_query)
+    await bot.send_message(user_id, text="Hi!\nI'm EchoBot!")
+
+# @dispatcher.message_handler(commands=["start", "help"])
+# async def send_welcome(message: types.Message):
+#     """
+#     This handler will be called when user sends `/start` or `/help` command
+#     """
+#     timestamp_now = datetime.now(tz=ZoneInfo("UTC")).isoformat(" ")
+#     insert_query = (f"INSERT INTO message (message, userid, message_time) "
+#                     f"VALUES ({message.text}, {message.from_user['id']}, {timestamp_now})")
+#     execute_query(insert_query)
+#     await message.reply("Hi!\nI'm EchoBot!")
+
+
+
 
 @dp.message_handler()
 async def echo(message: types.Message):
     " Функція відправки ехо-повідомлення"
     user_id = message.from_user.id
+    timestamp_now = datetime.now(tz=ZoneInfo("UTC")).isoformat(" ")
+    insert_query = (f"INSERT INTO message (message, userid, message_time) "
+                    f"VALUES ('{message.text}', {message.from_user['id']}, '{timestamp_now}')")
+    execute_query(insert_query)
     await bot.send_message(user_id, text=f"ви написали {message.text}")
-
-
-
-
 
 # @dispatcher.message_handler()
 # async def echo(message: types.Message):
@@ -46,3 +64,18 @@ async def echo(message: types.Message):
 #                     f"VALUES ('{message.text}', {message.from_user['id']}, '{timestamp_now}')")
 #     execute_query(insert_query)
 #     await message.answer(message.text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
